447f07e73c09495fe036505e89f72e8bd3e771b9
sagar <>
2022-03-08 06:07:39 +0530

    video number 5

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class HomeWidget extends StatefulWidget {
@@ -8,13 +10,46 @@ class HomeWidget extends StatefulWidget {
 }
 
 class _HomeWidgetState extends State<HomeWidget> {
+  List<String> people = [
+    "Heimin",
+    "Nannal",
+    "TibFox",
+    "CamStubs",
+    "Brish",
+    "Sagar"
+  ];
+
+  void addDateTimeStamp() {
+    var value = DateTime.now().toIso8601String();
+    setState(() {
+      people.add(value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('New Videos'),
+        title: const Text('New Videos from D.Tube'),
       ),
-      body: const Text('Hello World'),
+      body: ListView.separated(
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(people[index]),
+              leading: Image.asset('images/dtube_logo.png'),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return const Divider();
+          },
+          itemCount: people.length),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          log('Button Clicked');
+          addDateTimeStamp();
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
